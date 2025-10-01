acfa037ab969c08b83ad50ed984884ca8f4dbd49
meron6 <khozameron5@gmail.com>
2025-10-01 18:35:24 +0000

    update

--- alx-project-0x03/pages/index.tsx
+++ alx-project-0x03/pages/index.tsx
@@ -1,20 +1,3 @@
-// interface/index.ts
-import { ReactNode } from "react";
-
-export interface LayoutProps {
-  children: ReactNode;
-}
-
-export interface ButtonProps {
-  label: string;
-  onClick: () => void;
-}
-
-export interface PageRouteProps {
-  title: string;
-}
-tsx
-Copy code
 // pages/index.tsx
 import { PageRouteProps } from "@/interface";
 
@@ -23,31 +6,3 @@ const Home: React.FC<PageRouteProps> = ({ title }) => {
 };
 
 export default Home;
-tsx
-Copy code
-// components/common/Button.tsx
-import { ButtonProps } from "@/interface";
-
-const Button: React.FC<ButtonProps> = ({ label, onClick }) => {
-  return <button onClick={onClick}>{label}</button>;
-};
-
-export default Button;
-tsx
-Copy code
-// components/layouts/Layout.tsx
-import Footer from "./Footer";
-import Header from "./Header";
-import { LayoutProps } from "@/interface";
-
-const Layout: React.FC<LayoutProps> = ({ children }) => {
-  return (
-    <>
-      <Header />
-      <main>{children}</main>
-      <Footer />
-    </>
-  );
-};
-
-export default Layout;
